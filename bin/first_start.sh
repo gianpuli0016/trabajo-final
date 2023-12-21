0f6fb70a4bae029d6b2a58d7d16b1e2e0c30de98
gianpuli0016 <gianlucapulichino@gmail.com>
2023-12-21 14:30:32 -0300

    update

--- bin/first_start.sh
+++ bin/first_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose exec backend composer install
-docker-compose exec backend chmod 777 ./web/assets -R
-docker-compose exec backend chmod 777 ./runtime -R
+docker compose exec backend composer install
+docker compose exec backend chmod 777 ./web/assets -R
+docker compose exec backend chmod 777 ./runtime -R
 
